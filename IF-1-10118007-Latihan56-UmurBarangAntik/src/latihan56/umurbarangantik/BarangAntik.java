39179a5db27eab5a55d439a50a16370e625038c9
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-11-04 16:32:11 +0700

    Add files via upload

---
+++ IF-1-10118007-Latihan56-UmurBarangAntik/src/latihan56/umurbarangantik/BarangAntik.java
@@ -0,0 +1,13 @@
+package latihan56.umurbarangantik;
+public class BarangAntik {
+    
+    int umur;
+    
+    public BarangAntik (int umur){
+        this.umur = umur;
+    }
+    
+    public void tampilUmur (){
+        System.out.println("Umur barang antik ini adalah : "+umur+" tahun");
+    }
+}
---
+++ IF-1-10118007-Latihan56-UmurBarangAntik/src/latihan56/umurbarangantik/Radio.java
@@ -0,0 +1,19 @@
+package latihan56.umurbarangantik;
+public class Radio extends BarangAntik{
+    
+    private String name;
+    
+    public Radio(int umur) {
+        super(umur);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
